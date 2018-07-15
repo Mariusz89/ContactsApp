4501f78268a93cfa0b9aed0dedc9c739bd99e962
mariusz.kosior@onet.pl <mariusz.kosior@onet.pl>
2018-07-15 23:32:02 +0200

    build classic version application

---
+++ modern/src/view/main/Main.js
@@ -0,0 +1,41 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "mainView" property. That setting causes an instance of this class to be created and
+ * added to the Viewport container.
+ *
+ * TODO - Replace the content of this view to suit the needs of your application.
+ */
+Ext.define('ContactsApp.view.main.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'app-main',
+
+    requires: [
+        'Ext.MessageBox',
+        'ContactsApp.view.contacts.List1',
+        'ContactsApp.view.contacts.ContactsController',
+        'ContactsApp.view.contacts.ContactsModel'
+    ],
+
+    controller: 'main',
+    viewModel: 'main',
+
+    defaults: {   
+        tab: {
+            iconAlign: 'top'
+        },
+        styleHtmlContent: true
+    },
+
+    tabBarPosition: 'bottom',
+
+    items: [
+        {
+            title: 'Contacts',
+            iconCls: 'x-fa fa-users',
+            layout: 'fit',
+            items: [{
+                xtype: 'contactslist' //I added a component 'contactlist' inside the Form class
+            }]
+        }
+    ]
+});
