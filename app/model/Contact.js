1929b4f9d2a500cb426e53a157150bf002286450
mariusz.kosior@onet.pl <mariusz.kosior@onet.pl>
2018-07-15 23:07:57 +0200

    added files Main and Contact

---
+++ app/model/Contact.js
@@ -0,0 +1,22 @@
+Ext.define('ContactsApp.model.Contact', {
+    extend: 'Ext.data.Model',
+
+    requires: [
+        'Ext.data.proxy.LocalStorage',
+    ],
+
+    entityName: 'Contact',
+
+    fields: [
+        { name: 'firstName', type: 'string'},
+        { name: 'lastName', type: 'string'},
+        { name: 'phone', type: 'number' },
+        { name: 'email', type: 'string' },
+        { name: 'notes', type: 'string' },
+    ],
+
+    proxy: {
+        type: 'localstorage',
+        id  : 'contacts'
+    }
+});
---
+++ classic/src/view/main/Main.js
@@ -0,0 +1,91 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "mainView" property. That setting automatically applies the "viewport"
+ * plugin causing this view to become the body element (i.e., the viewport).
+ *
+ * TODO - Replace this content of this view to suite the needs of your application.
+ */
+Ext.define('ContactsApp.view.main.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'app-main',
+
+    requires: [
+        'Ext.plugin.Viewport',
+        'Ext.window.MessageBox',
+
+        'ContactsApp.view.main.MainModel',
+        'ContactsApp.view.main.MainController',
+        'ContactsApp.view.contacts.List'
+    ],
+
+    ui: 'navigation',
+
+    viewModel: 'main',
+    controller: 'main',
+
+    tabBarHeaderPosition: 1,
+    titleRotation: 0,
+    tabRotation: 0,
+
+    header: {
+        layout: {
+            align: 'stretchmax'
+        },
+        title: {
+            bind: {
+                text: '{name}'
+            },
+            flex: 0
+        },
+        iconCls: 'fa-th-list'
+    },
+
+    tabBar: {
+        flex: 1,
+        layout: {
+            align: 'stretch',
+            overflowHandler: 'none'
+        }
+    },
+
+    responsiveConfig: {
+        tall: {
+            headerPosition: 'top'
+        },
+        wide: {
+            headerPosition: 'left'
+        }
+    },
+
+    defaults: {
+        bodyPadding: 20,
+        tabConfig: {
+            plugins: 'responsive',
+            responsiveConfig: {
+                wide: {
+                    iconAlign: 'left',
+                    textAlign: 'left'
+                },
+                tall: {
+                    iconAlign: 'top',
+                    textAlign: 'center',
+                    width: 120
+                }
+            }
+        }
+    },
+
+    items: [{
+        title: 'Contacts',
+        iconCls: 'fa-users',
+        padding: '10',
+        items: [{
+            xtype: 'contactslist'
+        	}]
+        },
+        {
+        title: 'About',
+        iconCls: 'fa-info-circle',
+        html: 'contactslist xccxhcjhxjchjxhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhcxjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj'
+    }]
+});
